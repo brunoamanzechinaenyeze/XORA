2e06aa04830637617529a4f7e0df5ed36b8d3369
Amanze Bruno <heroicemblem69@gmail.com>
2024-09-23 00:01:41 +0100

    Finished styling the testimonial section

--- src/components/TestimonialItem.jsx
+++ src/components/TestimonialItem.jsx
@@ -7,6 +7,15 @@ const TestimonialItem = ({ item, containerClassName }) => {
       <blockquote className='h6 mb-8 text-p4'>
         {item.comment}
       </blockquote>
+      <div className='flex items-center max-xl:-mr-8'>
+        <div className='mr-4 size-20 shrink-0 rounded-half border-2 border-s2 p-1.5'>
+            <img src={item.avatarUrl} alt={item.name} className='size-full object-cover'/>
+        </div>
+        <div>
+            <h4 className='body-2 mb-0.5 text-p1'>{item.name}</h4>
+            <p className='small-compact uppercase text-s3'>{item.role}</p>
+        </div>
+      </div>
     </div>
   )
 }
