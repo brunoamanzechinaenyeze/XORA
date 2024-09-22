258e600abbacd27c2058648461a977f17f7e73a3
Amanze Bruno <heroicemblem69@gmail.com>
2024-09-22 20:20:24 +0100

    Added functionality to the view and close div in the faq section

---
+++ src/components/FaqItem.jsx
@@ -0,0 +1,41 @@
+import clsx from "clsx";
+import React, { useState } from "react";
+
+const FaqItem = ({ item, index }) => {
+  const [activeId, setActiveId] = useState(null);
+  const active = activeId === item.id;
+  return (
+    <div className="relative z-2 mb-16">
+      <div
+        className="group relative flex cursor-pointer justify-between gap-10 px-7"
+        onClick={() => {
+          setActiveId(active ? null : item.id);
+        }}
+      >
+        <div className="flex-1">
+          <div className="small-compact mb-1.5 text-p3 max-lg:hidden">
+            {index < 10 ? "0" : ""} {index}
+          </div>
+          <div
+            className={clsx(
+              "h6 text-p4 transition-colors duration-500 max-md:flex max-md:min-h-20 max-md:items-center",
+              active && "max-lg:text-p1"
+            )}
+          >
+            {item.question}
+          </div>
+        </div>
+        <div
+          className={clsx(
+            "faq-icon relative flex size-12 items-center justify-center rounded-full border-2 border-s2 shadow-400 transition-all duration-500 group-hover:border-s4",
+            active && "before:bg-p1 after:rotate-0 after:bg-p1"
+          )}
+        >
+          {/* <div className="g4 size-11/12 rounded-full shadow-300" /> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqItem;
--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { Element } from "react-scroll";
+import { faq } from '../constants/index'
+import FaqItem from "../components/FaqItem";
 
 const Faq = () => {
+ 
+const halfLength = Math.floor(faq.length/2)
+
   return (
     <section>
       <Element name="faq" className="relative">
@@ -17,7 +22,18 @@ const Faq = () => {
           <div className="faq-line_after w-0.5 h-full absolute left-[calc(50%-1px)] top-0 -z-1 bg-s2" />
           <div className="faq-glow_before relative z-2 border-2 border-s2 bg-s1">
             <div className="container flex gap-10 max-lg:block">
-              <div className="rounded-half absolute -top-10 left-[calc(50%-40px)] z-4 flex size-20 items-center justify-center border-2 border-s2 bg-s1"></div>
+              <div className="rounded-half absolute -top-10 left-[calc(50%-40px)] z-4 flex size-20 items-center justify-center border-2 border-s2 bg-s1">
+                <img
+                  src="/images/faq-logo.svg"
+                  alt="logo"
+                  className="size-1/2"
+                />
+              </div>
+              <div className="relative flex-1 pt-24">
+                {faq.slice(0, halfLength).map((item, index) => (
+                    <FaqItem key={item.id} item={item} index={index}/>
+                ))}
+              </div>
             </div>
           </div>
         </div>
