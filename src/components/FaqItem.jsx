db6494ea0e058984e8085a96d7b18b5308d573d8
Amanze Bruno <heroicemblem69@gmail.com>
2024-09-22 21:51:31 +0100

    Done with the first column of the FAQ section

--- src/components/FaqItem.jsx
+++ src/components/FaqItem.jsx
@@ -40,6 +40,11 @@ const FaqItem = ({ item, index }) => {
           { activeId === item.id && <div className="body-3 px-7 py-3.5"> {item.answer}
             </div>}
       </SlideDown>
+
+      <div className={clsx('g5 -bottom-7 -top-7 left-0 right-0 -z-1 rounded-3xl opacity-0 transition-opacity duration-500 absolute', active && 'opacity-100')}>
+        <div className="g4 absolute inset-0.5 -z-1 rounded-3xl" />
+        <div className="absolute left-8 top-0 h-0.5 w-40 bg-p1"/>
+      </div>
     </div>
   );
 };
