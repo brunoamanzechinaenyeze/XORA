8f4e02f4d6b5f746a01e584b7e920abeeb0150ef
Amanze Bruno <heroicemblem69@gmail.com>
2024-09-22 23:51:54 +0100

    Styling the testimonial section

--- src/components/TestimonialItem.jsx
+++ src/components/TestimonialItem.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import clsx from 'clsx'; // Ensure you're using this utility if required
 
 const TestimonialItem = ({ item, containerClassName }) => {
   return (
-    <div>TestimonialItem</div>
+    <div className={clsx('relative px-14 pt-11 pb-14 after:absolute after:bottom-0 after:right-0 after:h-0.5 after:w-screen after:bg-s2 after:content-[""] max-md:px-0 max-md:pt-11 after:max-md:-right-4', containerClassName)}>
+      <blockquote className='h6 mb-8 text-p4'>
+        {item.comment}
+      </blockquote>
+    </div>
   )
 }
 
