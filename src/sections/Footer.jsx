67f57d93f7dd49b30065dba7e8daabd86cd469d7
Amanze Bruno <heroicemblem69@gmail.com>
2024-09-23 01:53:33 +0100

    Done with the footer section

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import React from "react";
+import { socials } from "../constants/index";
 
 const Footer = () => {
   return (
     <footer>
-        <div className='container py-10'>
-
+      <div className="container py-10">
+        <div className="flex w-full max-md:flex-col">
+          <div className="small-compact flex flex-1 flex-wrap items-center justify-center gap-5">
+            <p className="opacity-70">Amanze Bruno</p>
+            <div className="flex items-center justify-center sm:ml-auto">
+              <p className="legal-after relative mr-9 text-p5 transtiton-all duration-500 hover:text-pl">
+                Privacy Policy
+              </p>
+              <p className="text-p5 transtiton-all duration-500 hover:text-pl">
+                Terms of use
+              </p>
+            </div>
+          </div>
+          <ul className="flex flex-1 justify-center gap-3 max-md:mt-10 md:justify-end">
+            {socials.map(({ id, url, icon, title }) => (
+              <li key={id}>
+                <a href={url} className="social-icon">
+                  <img
+                    src={icon}
+                    alt={title}
+                    className="size-1/3 object-contain"
+                  />
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
