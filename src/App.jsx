b63a838ea4d084397e3d72b9c77a2886e7f228b0
Amanze Bruno <heroicemblem69@gmail.com>
2024-09-23 01:38:42 +0100

    Created a new footer section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Pricing from "./sections/Pricing"
 import Faq from "./sections/Faq"
 import Testimonials from "./sections/Testimonials"
 import Download from "./sections/Download"
+import Footer from "./sections/Footer"
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
       <Faq />
       <Testimonials />
       <Download />
+      <Footer />
     </main>
   )
 }
