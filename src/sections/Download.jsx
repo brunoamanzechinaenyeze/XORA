f32a7345e7e464966d85aeb5e6dff043305b9186
Amanze Bruno <heroicemblem69@gmail.com>
2024-09-23 01:37:04 +0100

    added list of company logos

--- src/sections/Download.jsx
+++ src/sections/Download.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Element } from "react-scroll";
-import { links } from "../constants";
+import { links, logos } from "../constants";
 import Marker from "../components/Marker";
 
 const Download = () => {
@@ -52,14 +52,27 @@ const Download = () => {
             <div className="mb-10 max-md:hidden">
               <div className="download_preview-before download_preview-after rounded-40 relative w-[950px] border-2 border-s5 p-6">
                 <div className="relative rounded-3xl bg-s1 px-6 pb-6 pt-14">
-                    <span className="download_preview-dot left-6 bg-p2"/>
-                    <span className="download_preview-dot left-11 bg-s3"/>
-                    <span className="download_preview-dot left-16 bg-p1/15"/>
-                    <img src="/images/screen.jpg" />
+                  <span className="download_preview-dot left-6 bg-p2" />
+                  <span className="download_preview-dot left-11 bg-s3" />
+                  <span className="download_preview-dot left-16 bg-p1/15" />
+                  <img
+                    src="/images/screen.jpg"
+                    width={855}
+                    height={655}
+                    className="rounded-xl"
+                  />
                 </div>
               </div>
             </div>
           </div>
+
+          <ul className="mt-24 flex justify-center max-lg:hidden">
+            {logos.map(({ id, url, width, height, title }) => (
+                <li key={id} className="mx-10">
+                    <img src={url} width={width} height={height} alt={title} />
+                </li>           
+            ))}
+          </ul>
         </div>
       </Element>
     </section>
